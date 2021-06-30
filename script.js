71001cd55cb629470b4b2773b7ee3bd488d193ed
migrd <72756664+migrd@users.noreply.github.com>
2021-06-30 16:51:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,16 @@ VanillaTilt.init(document.querySelector(".container"), {
 });
 
 function equalButton(){
-  historyValue = document.querySelector('#history-value');
-  currentValue = document.calc.txt.value;
-
-
+  const historyValue = document.querySelector('#history-value');
+  const currentValue = document.calc.txt.value;
+  
   document.calc.txt.value = eval(calc.txt.value);
   historyValue.innerText = currentValue;
 }
 
 function clearDisplay(){
   calc.txt.value = '';
+
+  const historyValue = document.querySelector('#history-value');
   historyValue.innerText = '';
 }
