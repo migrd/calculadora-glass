7f8fd4152be905c0460cb5c93f14fbffde0198cc
migrd <72756664+migrd@users.noreply.github.com>
2021-06-29 23:42:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ VanillaTilt.init(document.querySelector(".container"), {
   max: 15,
   speed: 400,
   glare: true,
-  "max-glare": 1
+  "max-glare": 0.5
 });
+
+function equalButton(){
+  historyValue = document.querySelector('#history-value');
+  currentValue = document.calc.txt.value;
+
+
+  document.calc.txt.value = eval(calc.txt.value);
+  historyValue.innerText = currentValue;
+}
+
+function clearDisplay(){
+  calc.txt.value = '';
+  historyValue.innerText = '';
+}
